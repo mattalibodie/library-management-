3183a03d3c0199061670ba777c51338b9738b3bc
mattalibodie <nguyenluongtt2@gmail.com>
2025-01-21 10:51:42 +0700

    Thêm chức năng phân quyền, cấp quyền user, mã hoá mật khẩu, tạo token, xác thực jwt-oauth2

--- src/main/java/com/ebook/ebookproject/service/UserServiceImpl.java
+++ src/main/java/com/ebook/ebookproject/service/UserServiceImpl.java
@@ -15,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 
 import org.modelmapper.ModelMapper;
 import org.modelmapper.TypeMap;
-import org.springframework.core.env.PropertyResolver;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
